c1f8cdca93fc1999be2e076633b28f4edfd1421a
Rafał Jaworski <rafal.jaworski@bravelab.io>
2017-10-03 15:24:09 +0200

    Add data from POST requests

--- django_sentry_400_middleware/settings.py
+++ django_sentry_400_middleware/settings.py
@@ -2,3 +2,4 @@ from django.conf import settings
 
 
 IGNORED_USER_AGENTS = getattr(settings, 'SENTRY_404_IGNORED_USER_AGENTS', [])
+PROCESS_REQUEST_POST_DATA = getattr(settings, 'SENTRY_404_PROCESS_REQUEST_POST_DATA', True)
